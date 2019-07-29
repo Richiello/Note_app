188aff494376431c5f864b80d586850307af1ba7
richiello <richiello@hotmail.com>
2019-07-29 10:34:04 +0200

    Add initial state to note reducer

--- src/reducers/notes.js
+++ src/reducers/notes.js
@@ -1,16 +1,9 @@
-const INITIAL_DATA = [
-    {
-        id: 0,
-        text: 'Note 1',
-    },
-    {
-        id: 1,
-        text: 'Note 2',
+const INITIAL_STATE = {
+    id: 0,
+    text: 'My first note',
+}
 
-    },
-]
-
-const NoteReducer = (state = INITIAL_DATA, action) => {
+const notes = (state = [INITIAL_STATE], action) => {
     switch (action.type) {
         case 'ADD_NOTE':
             return [
@@ -18,21 +11,20 @@ const NoteReducer = (state = INITIAL_DATA, action) => {
                 {
                     id: action.id,
                     text: action.text,
-                    completed: false
                 }
             ]
         case 'EDIT_NOTE':
             return state.map(note =>
                 (note.id === action.id)
-                    ? { ...note, completed: !note.completed }
+                    ? { ...note, text: action.text }
                     : note
             )
-        case 'REMOVE_NOTE':
+        case 'DELETE_NOTE':
             const numIndex = parseInt(action.id)
-            return state.filter(note => note.id !== numIndex);
+            return state.filter(note => note.id !== numIndex)
         default:
             return state
     }
 }
 
-export default NoteReducer
+export default notes
