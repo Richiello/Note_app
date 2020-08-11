930f74b056d3247112827222e5bd959461989375
richiello <richiello@hotmail.com>
2020-08-11 09:57:35 +0200

    add trackers

--- src/containers/AddNote.js
+++ src/containers/AddNote.js
@@ -1,64 +1,104 @@
-import React, { useState } from 'react'
-import { connect } from 'react-redux'
-import { addNote } from '../actions'
-import Fab from '@material-ui/core/Fab'
-import AddIcon from '@material-ui/icons/Add'
-import { TextField, CardHeader, CardContent, Button } from '../styles/styles'
-import { categoryFilters } from '../actions'
-import ModalAdd from '../components/Modal'
-import moment from 'moment'
-
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { addNote } from "../actions";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import { TextField, CardHeader, CardContent, Button } from "../styles/styles";
+import { categoryFilters } from "../actions";
+import ModalAdd from "../components/Modal";
+import moment from "moment";
 
 const AddNote = ({ dispatch }) => {
-    const [category, setCategory] = useState('ALL')
-    const [showModal, setShowModal] = useState(false)
+    const [category, setCategory] = useState("ALL");
+    const [showModal, setShowModal] = useState(false);
 
     const onchange = (event) => {
-        setCategory(event.target.value)
-    }
+        setCategory(event.target.value);
+    };
 
     const toggleModal = () => {
-        setShowModal(!showModal)
-    }
+        setShowModal(!showModal);
+    };
     const addNewNote = (event) => {
-        let noteText = input.value
-        event.preventDefault()
+        let noteText = input.value;
+        event.preventDefault();
         if (!noteText.trim()) {
-            return
+            return;
         }
-        dispatch(addNote(noteText, category, newTimeStamp))
-        noteText = ''
-        toggleModal()
-    }
+        dispatch(addNote(noteText, category, newTimeStamp));
+        noteText = "";
+        toggleModal();
+    };
 
-    const newTimeStamp = moment().format('D MMM YYYY HH:mm')
+    const newTimeStamp = moment().format("D MMM YYYY HH:mm");
 
-    let input
+    let input;
 
     return (
         <>
-            <Fab onClick={() => toggleModal()} type='submit' color='secondary' variant='extended' aria-label='add'>
+            <Fab
+                onClick={() => {
+                    window._paq.push([
+                        "trackEvent",
+                        "Button Click",
+                        "Isa",
+                        "Member",
+                    ]);
+                    window._paq.push([
+                        "trackEvent",
+                        "Button Click",
+                        "Isas",
+                        "Member",
+                    ]);
+                    console.log(window._paq);
+                    alert = "Clicked";
+                    toggleModal();
+                }}
+                type="submit"
+                color="secondary"
+                variant="extended"
+                aria-label="add"
+            >
                 Add new note <AddIcon />
             </Fab>
-            {showModal && <ModalAdd show={toggleModal}>
-                <form style={{ width: '100%' }} onSubmit={e => addNewNote(e)}>
-                    <CardHeader>
-                        <p>Add new note:</p>
-                        <select onChange={e => onchange(e)} >
-                            <option value="" default> Choose category </option>
-                            {Object.keys(categoryFilters).map((o, index) =>
-                                <option key={index} value={categoryFilters[o]}>{categoryFilters[o]}</option>
-                            )}
-                        </select>
-                    </CardHeader>
-                    <CardContent>
-                        <TextField placeholder='* Text' resize='none' ref={node => (input = node)} />
-                        <Button type='submit'> Save </Button>
-                    </CardContent>
-                </form >
-            </ModalAdd>}
+            {showModal && (
+                <ModalAdd show={toggleModal}>
+                    <form
+                        style={{ width: "100%" }}
+                        onSubmit={(e) => addNewNote(e)}
+                    >
+                        <CardHeader>
+                            <p>Add new note:</p>
+                            <select onChange={(e) => onchange(e)}>
+                                <option value="" default>
+                                    {" "}
+                                    Choose category{" "}
+                                </option>
+                                {Object.keys(categoryFilters).map(
+                                    (o, index) => (
+                                        <option
+                                            key={index}
+                                            value={categoryFilters[o]}
+                                        >
+                                            {categoryFilters[o]}
+                                        </option>
+                                    )
+                                )}
+                            </select>
+                        </CardHeader>
+                        <CardContent>
+                            <TextField
+                                placeholder="* Text"
+                                resize="none"
+                                ref={(node) => (input = node)}
+                            />
+                            <Button type="submit"> Save </Button>
+                        </CardContent>
+                    </form>
+                </ModalAdd>
+            )}
         </>
-    )
-}
+    );
+};
 
-export default connect()(AddNote)
+export default connect()(AddNote);
