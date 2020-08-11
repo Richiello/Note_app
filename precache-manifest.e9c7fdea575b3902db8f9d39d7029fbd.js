f857604d3169c6ef6aa7ba5eaf8e8402aead705f
Isabella Richiello <isabella.richiello@detectify.com>
2020-08-11 09:53:59 +0200

    Updates

---
+++ precache-manifest.e9c7fdea575b3902db8f9d39d7029fbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ec5a82db68307df91c4964e4f3072b",
+    "url": "/Note_app/index.html"
+  },
+  {
+    "revision": "ac4bf3427a2ebc1818a5",
+    "url": "/Note_app/static/css/main.82bd4aa1.chunk.css"
+  },
+  {
+    "revision": "7daab793889966aa908e",
+    "url": "/Note_app/static/js/2.f392821b.chunk.js"
+  },
+  {
+    "revision": "ac4bf3427a2ebc1818a5",
+    "url": "/Note_app/static/js/main.66ac51b8.chunk.js"
+  },
+  {
+    "revision": "4d0666f3fbecd6aaf1ae",
+    "url": "/Note_app/static/js/runtime~main.6bb5d484.js"
+  }
+]);
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import notes from './notes'
-import categoryFilter from './categories'
-
-export default combineReducers({
-    notes,
-    categoryFilter
-})
